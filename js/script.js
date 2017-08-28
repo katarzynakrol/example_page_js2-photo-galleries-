a444c0200eea89e8c761568ff1d85ae521547449
katarzynaarmatys <armatys.k@gmail.com>
2017-08-28 15:08:12 +0200

    Utworzenie repozytorium

---
+++ js/script.js
@@ -0,0 +1,58 @@
+function navbarFunction() {
+    var x = document.getElementById('topNav');
+
+    if (x.className === 'navbar') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navbar';
+    }
+}
+
+function showBigPicture(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
+function hideBigPicture(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode == 27) {
+        var modals = document.getElementsByClassName('modal');
+        for (var i = 0; i < modals.length; i++) {
+            modals[i].style.display = 'none';
+        }
+    }
+});
+
+window.onload = changeSlide;
+
+var number = Math.floor(Math.random() * 4) + 1;
+
+var timer1 = 0;
+
+function currentSlide(n) {
+    clearTimeout(timer1);
+    number = n - 1;
+    changeSlide();
+}
+
+function plusSlides(n) {
+    clearTimeout(timer1);
+    number += n;
+    if (number < 0) {
+        number = 3;
+    }
+    changeSlide();
+}
+
+function changeSlide() {
+    number++;
+    if (number > 4) {
+        number = 1;
+    }
+    var image = '<img src=\"images/gallery' + number + '.jpg\">';
+    document.getElementById('slideshow').innerHTML = image;
+
+    timer1 = setTimeout('changeSlide()', 3000);
+}
